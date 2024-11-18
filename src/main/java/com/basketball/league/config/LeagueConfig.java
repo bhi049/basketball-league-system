634686e719873ae736e7ce154db93616dea7e39d
bhi049 <bhi049@myy.haaga-helia.fi>
2024-11-18 12:57:24 +0200

    LeagueConfig Deleted

--- src/main/java/com/basketball/league/config/LeagueConfig.java
+++ src/main/java/com/basketball/league/config/LeagueConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 @Configuration
-@DependsOn("teamDataInitializer")
 public class LeagueConfig {
 }
 
